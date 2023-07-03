ead4476a95f7f41467686283b53e41292cdbac3b
Arkaenaz <flooper2234@gmail.com>
2023-07-03 17:35:33 +0800

    added scene tag

---
+++ Model/Enum/EnumSceneTag.hpp
@@ -0,0 +1,11 @@
+#ifndef ENUM_SCENE_TAG_HPP
+#define ENUM_SCENE_TAG_HPP
+
+enum class SceneTag : int {
+    TITLE_SCREEN = 0,
+    GAME_SPACE,
+    // Additional scenes that split the game space?
+    LEADERBOARDS
+};
+
+#endif
