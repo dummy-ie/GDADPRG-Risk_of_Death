7284b6a13dfa078fa73a1f9905a3a014bfcf9abe
Arkaenaz <flooper2234@gmail.com>
2023-07-27 06:23:25 +0800

    temporary

---
+++ Model/Component/Script/Killable.hpp
@@ -0,0 +1,40 @@
+#ifndef COMPONENTS_KILLABLE_HPP
+#define COMPONENTS_KILLABLE_HPP
+
+#include "../../../Controller/Singleton/TextureManager.hpp"
+
+#include "../../Enum/EnumAnimationType.hpp"
+#include "../../Enum/EnumAssetType.hpp"
+#include "../../Enum/EnumComponentType.hpp"
+#include "../../Pooling/PoolableObject.hpp"
+#include "../../AnimatedTexture.hpp"
+
+#include "../Component.hpp"
+
+namespace models {
+    class Enemy;
+}
+
+namespace components {
+    using namespace controllers;
+    using namespace models;
+
+    class Killable : public Component {
+        private:
+            float fFrameInterval;
+            float fTicks;
+            bool bKilled;
+
+        public:
+            Killable(std::string strName, float fFrameInterval);
+
+        public:
+            void perform();
+        
+        public:
+            bool isKilled();
+            void setKilled(bool bKilled);
+    };
+}
+
+#endif
